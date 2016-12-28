cd5b70a417be49fe382f55af11be42c58e76ccb4
jamesdlc <jamesdelacruz@gmail.com>
2016-12-27 19:19:01 -0800

    real first commit

--- app/app.jsx
+++ app/app.jsx
@@ -3,12 +3,14 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 
+
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 // requiring app css
 require('style!css!sass!applicationStyles')
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
